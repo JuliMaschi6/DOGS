1d52a4bb3af67de47daf5c4e38d3e4a86b2e42c4
Juliana <julimaschi@gmail.com>
2022-07-10 12:55:20 -0300

    landign background

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,19 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../reducer/index";
-import thunk from "redux-thunk";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import rootReducer from "../reducer/index";
+// import thunk from "redux-thunk";
 
-const store = createStore(
-    rootReducer,
-    compose( applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-)
+// const store = createStore(
+//     rootReducer,
+//     compose( applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// )
 
-export default store;
+// export default store;
+
+import {createStore, applyMiddleware} from "redux"
+import {composeWithDevTools} from "redux-devtools-extension"
+import thunk from "redux-thunk"
+import rootReducer from "../reducer"
+
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
